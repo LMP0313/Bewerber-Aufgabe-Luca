79fb9a31c813809b5d011b37495bb7aa07104818
Stanzky <137049422+StanzkySTEP@users.noreply.github.com>
2024-07-16 18:12:05 +0200

    commit

---
+++ FormMenu.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class FormMenu : Form
+    {
+        private System.Windows.Forms.DataGridView Customer_Table;
+        private System.Windows.Forms.Label Dashboard_Label;
+        private System.Windows.Forms.Label Kunden_Label;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
+            Dashboard_Label = new Label();
+            Kunden_Label = new Label();
+            Customer_Table = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)Customer_Table).BeginInit();
+            SuspendLayout();
+            // 
+            // Dashboard_Label
+            // 
+            Dashboard_Label.AutoSize = true;
+            Dashboard_Label.FlatStyle = FlatStyle.Flat;
+            Dashboard_Label.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
+            Dashboard_Label.Location = new Point(12, 9);
+            Dashboard_Label.Name = "Dashboard_Label";
+            Dashboard_Label.Size = new Size(119, 30);
+            Dashboard_Label.TabIndex = 0;
+            Dashboard_Label.Text = "Dashboard";
+            // 
+            // Kunden_Label
+            // 
+            Kunden_Label.AutoSize = true;
+            Kunden_Label.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            Kunden_Label.Location = new Point(12, 50);
+            Kunden_Label.Name = "Kunden_Label";
+            Kunden_Label.Size = new Size(66, 21);
+            Kunden_Label.TabIndex = 1;
+            Kunden_Label.Text = "Kunden";
+            // 
+            // Customer_Table
+            // 
+            Customer_Table.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Customer_Table.Location = new Point(12, 74);
+            Customer_Table.Name = "Customer_Table";
+            Customer_Table.Size = new Size(461, 338);
+            Customer_Table.TabIndex = 2;
+            // 
+            // FormMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Customer_Table);
+            Controls.Add(Kunden_Label);
+            Controls.Add(Dashboard_Label);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FormMenu";
+            Text = "TecPets GmbH";
+            Load += FormMenu_Load;
+            ((System.ComponentModel.ISupportInitialize)Customer_Table).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ FormMenu.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class FormMenu : Form
+    {
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sboeger\Repositories\Aera\Bewerber-Aufgabe\DB\Database.mdf;Integrated Security=True";
+        private readonly string query = "SELECT CustomerID, FirstName, LastName, Email FROM Customer";
+
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    Customer_Table.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Data Load Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
